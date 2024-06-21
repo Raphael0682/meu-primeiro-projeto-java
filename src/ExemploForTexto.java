c8ff155b72114b405872565f408ee2b46049aef8
Raphael0682 <raphael.sanches.net@gmail.com>
2024-06-21 18:05:38 -0300

    Novas classes adicionadas

---
+++ src/ExemploForTexto.java
@@ -0,0 +1,8 @@
+public class ExemploForTexto {
+    public static void main(String[] args) {
+        String nome = "VishMaria";
+        for (int x = 0; x <= nome.length(); x++) {
+            System.out.println("Letra:" + nome.charAt(x) + " está na posiçao " + x);
+        }
+    }
+}
