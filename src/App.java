ee869c76a6c40f3bdbeedd9707712d0bf4d254a2
Raphael0682 <raphael.sanches.net@gmail.com>
2024-06-27 20:14:25 -0300

    Atualizações dos arquivos

--- src/App.java
+++ src/App.java
@@ -1,10 +1,9 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner TextoDigitado = new Scanner(System.in);
-        System.out.println("Digite um texto qualquer: ");
-        final String texto = TextoDigitado.nextLine();
-        System.out.println("Você digitou: " + texto);
-    }
+        if (condition) {
+            
+        } else {
+            
+        }
+     }
 }
---
+++ src/ContadorLongo.java
@@ -0,0 +1,7 @@
+public class ContadorLongo {
+    public static void main(String[] args) {
+        for (int x = 0; x <= 10000; x++) {
+            System.out.print(x + " ");
+        }
+    }
+}
